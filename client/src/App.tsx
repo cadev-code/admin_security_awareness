cd28b525cac8a6ed92173a2a039efa1917474de7
Carlos Alberto <carlosdev1810@gmail.com>
2025-11-24 11:44:27 -0600

    feat(client): implement GlobalAlert component and alert state management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import { GlobalAlert } from './components';
 import { AppRoutes } from './routes';
 
 export const App = () => {
   return (
     <>
       <AppRoutes />
+      <GlobalAlert />
     </>
   );
 };
