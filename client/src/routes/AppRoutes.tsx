0428168f1b8437c53428772ca662422d4ad5089e
Carlos Alberto <carlosdev1810@gmail.com>
2025-11-25 12:11:15 -0600

    feat(client): implement PublicRoute and ProtectedRoute components for route protection

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,13 +1,26 @@
 import { Navigate, Route, Routes } from 'react-router';
 import { Approved, Login } from '@/pages';
+import { PublicRoute } from './PublicRoute';
+import { ProtectedRoute } from './ProtectedRoute';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="approved" element={<Approved />} />
+      <Route element={<PublicRoute />}>
+        <Route path="login" element={<Login />} />
 
-      <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route index element={<Navigate to="/login" replace />} />
+
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Route>
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="approved" element={<Approved />} />
+
+        <Route index element={<Navigate to="/approved" replace />} />
+
+        <Route path="*" element={<Navigate to="/approved" replace />} />
+      </Route>
     </Routes>
   );
 };
---
+++ client/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,25 @@
+import { useCurrentUser } from '@/hooks';
+import { Loader2 } from 'lucide-react';
+import { Navigate, Outlet } from 'react-router';
+
+export const ProtectedRoute = () => {
+  const { data: user, isLoading, isError } = useCurrentUser();
+
+  if (isLoading) {
+    return (
+      <div className="w-[100vw] h-[100vh] flex items-center justify-center text-gray-600">
+        <Loader2 className="h-12 w-12 animate-spin" />
+      </div>
+    );
+  }
+
+  if (isError || !user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+};
