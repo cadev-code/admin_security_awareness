ed8ef7cc18d1567467f5dbe533278eee8b72ecf1
Carlos Alberto <carlosdev1810@gmail.com>
2025-11-25 12:06:44 -0600

    feat(server): add auth/me endpoint

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,10 +1,16 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import { login } from '../controllers';
-import { validateInput } from '../middlewares';
+import { authMiddleware, validateInput } from '../middlewares';
 import { loginSchema } from '../schemas';
 
 const router = Router();
 
 router.post('/auth/login', validateInput(loginSchema), login);
+
+router.get('/auth/me', authMiddleware, (req: Request, res: Response) => {
+  res.json({
+    ...req.user,
+  });
+});
 
 export default router;
