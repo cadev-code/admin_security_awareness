185cfc09f4241b80f828649e51aaa92a13975d11
Carlos Alberto <carlosdev1810@gmail.com>
2025-12-04 14:58:32 -0600

    feat(client): add types for modules and modules response

---
+++ client/src/types/modules.types.ts
@@ -0,0 +1,14 @@
+interface Module {
+  id: number;
+  title: string;
+  type: string;
+  url: string;
+  bgColor: string;
+  bgImage: string;
+  logo: string;
+}
+
+export interface ModulesResponse {
+  error: string | null;
+  data: Module[];
+}
