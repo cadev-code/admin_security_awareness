46e4368fcf4fe1ce6662ab186430fc89fe446295
Carlos Alberto <carlosdev1810@gmail.com>
2025-12-04 14:59:01 -0600

    feat(client): enhance Management page to display modules with action buttons

--- client/src/pages/Management/Management.tsx
+++ client/src/pages/Management/Management.tsx
@@ -6,12 +6,17 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { LayoutTemplate, Plus } from 'lucide-react';
+import { GripVertical, LayoutTemplate, Plus } from 'lucide-react';
 import { AddModule } from './components';
 import { useState } from 'react';
+import { useModules } from '@/hooks';
 
 export const Management = () => {
   const [showAddModule, setShowAddModule] = useState(false);
+
+  const { data, isPending } = useModules();
+
+  console.log(data);
 
   return (
     <div className="min-h-screen w-full">
@@ -47,10 +52,33 @@ export const Management = () => {
                     size="sm"
                     className="cursor-pointer"
                     onClick={() => setShowAddModule(true)}
+                    disabled={isPending}
                   >
                     <Plus />
                     Agregar Módulo
                   </Button>
+                </div>
+                <div className="flex flex-col gap-2">
+                  {data?.data.map((module) => (
+                    <div className="flex flex-row gap-2 items-center">
+                      <GripVertical className="text-gray-500 cursor-grab" />
+                      <div className="border rounded px-4 py-2 bg-background w-full flex items-center gap-4">
+                        <h3 className="font-medium">{module.title}</h3>
+                        <Button
+                          size="sm"
+                          className="cursor-pointer bg-gray-600 hover:bg-gray-500"
+                        >
+                          Gestionar Contenido
+                        </Button>
+                        <Button
+                          size="sm"
+                          className="cursor-pointer bg-gray-600 hover:bg-gray-500"
+                        >
+                          Editar Módulo
+                        </Button>
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
             </CardContent>
