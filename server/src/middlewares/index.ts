334e2fb9b3a75d299c960452cce2dbf191af9165
Carlos Alberto <carlosdev1810@gmail.com>
2025-12-11 16:40:26 -0600

    feat(server): add validateParam middleware for request parameter validation

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -2,4 +2,5 @@ export * from './authMiddleware';
 export * from './errorHandler';
 export * from './uploadMiddleware';
 export * from './validateInput';
+export * from './validateParam';
 export * from './validateQuery';
